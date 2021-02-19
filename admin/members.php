45521590a79be848a89362d889c86a70ab4b21de
M ALZEN <mszen579@hotmail.com>
2021-02-19 18:46:30 +0100

    v19221

--- admin/members.php
+++ admin/members.php
@@ -10,25 +10,98 @@
 2- If: there is a user logged in then  include the init.php and footer.php
 3- Else: redirect to index.php
 */
-
+//always start with session
 session_start();
+//adding page title
+$pageTitle = 'Members';
+
 if(isset($_SESSION['username'])){
 
     include 'init.php';
 
     // like what we have done in the page.php page
     $do = isset($_GET['do']) ? $_GET['do'] : 'manage';
+
     // start the manage page
     if ($do == 'manage'){
        // manage page;
-    }elseif($do == 'Edit'){
-        //edit page;
-        echo 'welcome to members edit page' . 'your id is: ' . $_GET['userid'];    
-    }
+
+    }elseif($do == 'Edit'){//edit page; 
+        // forcing number for do=userid
+        $userid = isset($_GET['userid']) && is_numeric($_GET['userid']) ? intval($_GET['userid']) : 0;
+        // query from db
+        $stmt = $con->prepare('SELECT * FROM users WHERE userid=? LIMIT 1');
+        // execute the above query
+        $stmt -> execute(array($userid));
+        // we need to fetch the logged in user to edit his/her information 
+        $row = $stmt -> fetch();
+        // I use the rowcount to fetch the data if there is an id in the db
+        $count = $stmt->rowCount();
+        //incase if the user is available we will do the below action
+        if($stmt->rowCount() > 0){?>      
+            <!-- starting html to add a form -->
+            <h1 class='text-center'>Edit Member</h1>
+            <div class="container">
+            <form>
+            <!--  start add username -->
+            <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">username</label>
+            <div class="col-sm-10 col-md-8">
+            <input type="text" name='username' class="form-control"  placeholder="<?php echo $row['username'];?>" autocomplete='off'>
+            </div>
+            </div><br>
+            <!--  End add username -->
+            
+            <!--  start add Password -->
+            <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10 col-md-8">
+            <input type="password" name='password' class="form-control"  placeholder="Password" autocomplete='new-password'>
+            </div>
+            </div><br>
+            <!--  End add Password -->
+            
+            <!--  start add Email -->
+            <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10 col-md-8">
+            <input type="email" name='email' class="form-control"  placeholder="<?php echo $row['email'];?>">
+            </div>
+            </div><br>
+            <!--  End add Email -->
+            
+            <!--  start add Full name -->
+            <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Full Name</label>
+            <div class="col-sm-10 col-md-8">
+            <input type="text" name='fullname' class="form-control"  placeholder="<?php echo $row['fullname'];?>">
+            </div>
+            </div><br>
+            <!--  End add Full name -->
+            
+            <!--  start add Submit -->
+            <div class="form-group row">
+            <div class="col-sm-offset-2 col-sm-10">
+            <input type="submit" value='save' class="btn btn-primary"  placeholder="Full Name">
+            </div>
+            </div>
+            <!--  End add Submit -->
+            
+            </form>
+            </div>
+        <?php    
+        }else{
+            echo 'there is on such user to edit';
+        }?>
+            
+    
+
+<?php
+   }
     include $tpl . "footer.php";
 
-}else{
-    //to redirect non authorized users to index page
-    header ('location: index.php'); //route
-    exit();
-}
+    }else{
+        //to redirect non authorized users to index page
+        header ('location: index.php'); //route
+        exit();
+    }
